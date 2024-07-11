a2182e8e42f7171f1ac677eebf14143fe9b49d0f
itz-amethyst <pransermi@gmail.com>
2024-07-11 18:54:00 +0330

    fix: base

--- common/models/base.py
+++ common/models/base.py
@@ -1,6 +1,6 @@
 from common.models.session import Base
 from datetime import datetime
-from sqlalchemy import Column, DateTime
+from sqlalchemy import Column, DateTime, Integer
 
 def date_time_sec():
     return datetime.now().replace(microsecond=0)
@@ -9,5 +9,6 @@ def date_time_sec():
 class BaseModel(Base):
     __abstract__ = True
     
+    id = Column(Integer, primary_key=True, index=True)
     created_time = Column(DateTime, default=date_time_sec, nullable=False)
     modified = Column(DateTime, default=date_time_sec, onupdate=date_time_sec, nullable=True)
