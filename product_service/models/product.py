b1352cf0abc6a0fe9618c32162c9c0d6bb57c554
itz-amethyst <pransermi@gmail.com>
2024-07-11 22:34:46 +0330

    feat: product table

---
+++ product_service/models/product.py
@@ -0,0 +1,52 @@
+from sqlalchemy import Column, Float, String, Text
+from common.models.base import BaseModel
+from common.models.pictureMixIn import PictureMixin
+from slugify import slugify 
+class Product(BaseModel, PictureMixin):
+    
+    __tablename__ = "products"
+    
+    name = Column(String, unique=True, index=True, nullable=False)
+    slug = Column(String, unique=True, nullable=False)
+    description = Column(Text, nullable=True)
+    price = Column(Float, nullable=False)
+    
+
+    def __init__(self, name , **kwargs):
+        self.name = name
+        self.slug = self.generate_slug(name)
+        super().__init__(**kwargs)
+        
+    def generate_slug(self, name):
+        return slugify(name)
+
+    def __repr__(self):
+        return f"Product: name={self.name}, price={self.price}"
+
+
+
+
+
+
+# Usage 
+# async def create_product_with_image():
+#     async with DBSessionManager().session_async() as session:
+#         # Create a new product
+#         new_product = Product(name="Sample Product", description="This is a sample product.", price=100)
+#         session.add(new_product)
+#         await session.commit()
+
+#         # Ensure the PictureMixin setup is done
+#         Product.setup(session)
+
+#         # Create a generic picture for the product
+#         new_image = GenericPicture(
+#             picture_url="http://example.com/image.jpg", 
+#             content_type_id=Product.content_type_id, 
+#             object_id=new_product.id
+#         )
+#         session.add(new_image)
+#         await session.commit()
+
+# # Running the async function
+# asyncio.run(create_product_with_image())
