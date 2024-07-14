be121d95f3b9ab20685416f80e4e0be57df81749
itz-amethyst <pransermi@gmail.com>
2024-07-14 20:47:32 +0330

    feat: order model

---
+++ order_service/models/order.py
@@ -0,0 +1,24 @@
+from sqlalchemy import ForeignKey, Integer, Float, Enum as SQLAEnum
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID 
+from common.models.base import BaseModel
+from user_service.models.user import User
+from enum import Enum
+import uuid
+
+class OrderStatus(Enum):
+    PENDING = "Pending"
+    CANCLED = "Cancled"
+    ACCEPTED = "Accepted"
+
+
+class Order(BaseModel):
+    # Later can change the ref alghorithm
+    reference_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
+    total_amount: Mapped[float] = mapped_column(Float, nullable=False) 
+    status: Mapped[OrderStatus] = mapped_column(SQLAEnum(OrderStatus), default=OrderStatus.PENDING, nullable=False) 
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), nullable=False)
+    user: Mapped[User] = relationship("User", back_populates="orders", uselist=False)
+    
+    # ?! uselist=?!
+    user = relationship(User, back_populates="orders", uselist=False)
