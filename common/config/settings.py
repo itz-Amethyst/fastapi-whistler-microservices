73d6c58245f16274f7386ac8f92c15b4463ea90a
itz-amethyst <pransermi@gmail.com>
2024-07-14 20:48:21 +0330

    refactor

--- common/config/settings.py
+++ common/config/settings.py
@@ -1,15 +1,17 @@
 from pydantic_settings import BaseSettings
 from functools import lru_cache
 from pydantic import SecretStr
-from common.config.helper import config
+from common.config.helper import DOTENV
 
 class Settings(BaseSettings):
-    POSTGRES_USER: str = config.get("POSTGRES_USER")
-    POSTGRES_PASSWORD: SecretStr = config.get("POSTGRES_PASSWORD")
-    POSTGRES_DB: str = config.get("POSTGRES_DB")
-    FULL_DATABASE_PG_URL: str = config.get("FULL_DATABASE_PG_URL")
+    POSTGRES_USER: str 
+    POSTGRES_PASSWORD: SecretStr 
+    POSTGRES_DB: str 
+    FULL_DATABASE_PG_URL: str 
 
 
+    class Config:
+        env_file = DOTENV
 
 @lru_cache(maxsize=128)
 def get_settings() -> Settings:
