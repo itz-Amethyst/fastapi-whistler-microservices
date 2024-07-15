5a30d1bf9cff0a97cc7fe22874ccb6d32b5ed37b
itz-amethyst <pransermi@gmail.com>
2024-07-15 22:53:30 +0330

    feat: insert_product method

--- product_service/repository/product.py
+++ product_service/repository/product.py
@@ -1,11 +1,13 @@
-from typing import List
+from typing import List, Optional
 from fastapi import UploadFile
 from slugify import slugify
 from sqlalchemy import delete, update, insert, text
 from sqlalchemy.future import select
 from sqlalchemy.orm import Session
 from datetime import datetime
-from product_service.models.product import Product
+from common.schemas.image import Image
+from product_service.models.product import Product, Product
+from common.repository.genericPicture import GenericPictureRepository
 
 class ProductRepository:
     
@@ -13,6 +15,32 @@ class ProductRepository:
         self.session: Session = sess
 
 
+    #fix seller_id later to retrieve real data
+    async def insert_product(self, seller_id: int, product, picture: UploadFile = None) -> bool:
+        try:
+            product.slug = slugify(product.name)
+            sql = text(
+                """
+                    insert into products (name, slug, description, price, seller_id)
+                    values(:name, :slug, :description, :price, :seller_id)
+                    returning id
+                """) 
+            result = await self.session.execute(sql,{**product , "seller_id": seller_id})
+            product_id = result.fetchone()[0]
+            
+            if picture:
+                generic_repo = GenericPictureRepository(session= self.session)
+                generic_repo.add_picture(product_id, picture, model_name = Product.__tablename__)
+
+            await self.session.commit()
+
+            data = await self.get_product_with_pictures(product_id)
+            return data, True
+        except Exception as e :
+            await self.session.rollback()
+            print(f"Error while creating product: {e}")
+            return False
+            
     async def get_all_products(self, skip:int = 0 , limit:int = 10) ->List[Product]:
         try:
             sql = text("select * from products order by id offset :skip limit :limit")
@@ -21,4 +49,51 @@ class ProductRepository:
             return [Product(**product) for product in products]
         except Exception as e:
             print(f"Error fetching data: {e}")
-            return 
+            return [] 
+
+
+    async def get_product_with_pictures(self, product_id: int) -> Optional[Product]:
+        try:
+            sql = text(
+                """
+                select p.id, p.name, p.slug, p.descriptionSELECT p.id, p.name, p.slug, p.description, p.price, p.seller_id,
+                       gp.picture_url, gp.content_type_id, gp.object_id
+                from products p
+                left join generic_pictures gp on p.id = gp.object_id
+                where p.id = :product_id
+                """
+            )
+            
+            result = await self.session.execute(sql, {"product_id": product_id})
+            rows = result.fetchall()
+
+            if not rows:
+                return None
+            
+            product_data = rows[0]
+            
+            product_dict = {
+                "id": product_data["id"],
+                "name": product_data["name"],
+                "slug": product_data["slug"],
+                "description": product_data["description"],
+                "price": product_data["price"],
+                "seller_id": product_data["seller_id"],
+                "images": []
+            }
+            
+            for row in rows:
+                if row['picture_url']:
+                    image = Image(
+                        picture_url = row['picture_url'],
+                        content_type_id=row["content_type_id"],
+                        object_id = row['object_id']
+                    )
+                    product_dict['images'].append(image)
+                    
+            return Product(**product_dict)
+        except Exception as e:
+            await self.session.rollback()
+            print("Something went wrong while fetching product with pictures: {e}")
+            return None
+        
