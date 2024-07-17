b8d11d7e202075a7cf7fc2368d89ef43a94bb9a4
itz-amethyst <pransermi@gmail.com>
2024-07-17 12:41:14 +0330

    feat: get all products and endpoints

--- product_service/api/product.py
+++ product_service/api/product.py
@@ -23,3 +23,28 @@ async def create_product(req: Request, product: ProductCreate, picture: UploadFi
         raise HTTPException(status_code = 400, detail= "Error while creating a product")
 
     return product_result
+
+@router.get("/products/{product_id}", response_model=Product)
+async def get_product_by_id(product_id :int, db: AsyncSession = Depends(DBSessionDepAsync)):
+    repo = ProductRepository(db)
+    product = await repo.get_product_by_id(product_id)
+    if not product:
+        raise HTTPException(status=404, detail="Product with id {product_id} not found")
+    return product
+
+@router.get("/products/{product_slug}", response_model=Product)
+async def get_product_by_slug(product_slug : str, db: AsyncSession = Depends(DBSessionDepAsync)):
+    repo = ProductRepository(db)
+    product = await repo.get_product_by_slug(product_slug)
+    if not product:
+        raise HTTPException(status=404, detail="Product with id {product_id} not found")
+    return product
+
+
+@router.get("/products/", response_model=List[Product])
+async def get_all_products_with_picture(db: AsyncSession = Depends(DBSessionDepAsync)):
+    repo = ProductRepository(db)
+    products = await repo.get_all_products_with_pictures()
+    if not products:
+        raise HTTPException(status=404, detail="Product with id {product_id} not found")
+    return products
