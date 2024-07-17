4301d38bc00960e888d3574080fecb22e800cfbd
itz-amethyst <pransermi@gmail.com>
2024-07-17 18:39:19 +0330

    feat: update && get_id endpoints

--- order_service/api/order.py
+++ order_service/api/order.py
@@ -24,10 +24,28 @@ async def create_product(req: Request, order: OrderCreate, db: AsyncSession = De
 
     return created_order 
 
+@router.put("/order/{order_id}", response_model=Order)
+async def update_order(order_id: int, order: OrderUpdate, db: AsyncSession = Depends(DBSessionDepAsync)):
+    repo = OrderRepository(db)
+    updated_order = repo.update_order(order_id, order)
+        
+    if not updated_order:
+        raise HTTPException(status=404, detail=f"Order with id {order_id} not found")
+    
+    return updated_order
+
 @router.get("/orders/{order_id}", response_model=Order)
 async def get_order_by_id(order_id :int, db: AsyncSession = Depends(DBSessionDepAsync)):
     repo = OrderRepository(db)
     order = await repo.get_order_by_id(order_id)
     if not order:
         raise HTTPException(status=404, detail=f"Order with id {order_id} not found")
+    return order 
+
+@router.get("/orders/{reference_id}", response_model=Order)
+async def get_order_by_reference_id(reference_id : str, db: AsyncSession = Depends(DBSessionDepAsync)):
+    repo = OrderRepository(db)
+    order = await repo.get_order_by_reference_id(reference_id)
+    if not order:
+        raise HTTPException(status=404, detail=f"Order with reference id {reference_id} not found")
     return order 
