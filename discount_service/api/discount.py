d47f9b1a0d6f4cb7897df047845cb7a6d1ead272
itz-amethyst <pransermi@gmail.com>
2024-07-20 23:54:36 +0330

    feat: crud discount apis

---
+++ discount_service/api/discount.py
@@ -0,0 +1,41 @@
+from typing import List
+from fastapi import APIRouter, HTTPException
+from discount_service.repository.discount import DiscountRepository
+from discount_service.models import Discount as DiscountModel
+from discount_service.schemas import CreateDiscount, UpdateDiscount, Discount
+
+
+router = APIRouter()
+repository = DiscountRepository()
+
+@router.post("/discounts/", response_model=bool)
+async def create_discount(discount: CreateDiscount):
+    discount = DiscountModel(**discount.model_dump())
+    print(discount)
+    return await repository.create(discount)
+
+@router.get("/discounts/{discount_id}", response_model=Discount)
+async def read_discount(discount_id: str):
+    discount = await repository.get(discount_id)
+    if not discount:
+        raise HTTPException(status_code=404, detail="Discount not found")
+    return discount
+
+@router.put("/discounts/{discount_id}", response_model=Discount)
+async def update_discount(discount_id: str, update_data: UpdateDiscount):
+    update_dict = update_data.model_dump(exclude_unset=True)
+    discount = await repository.update(discount_id, update_dict)
+    if not discount:
+        raise HTTPException(status_code=404, detail="Discount not found")
+    return discount
+
+@router.delete("/discounts/{discount_id}", response_model=dict)
+async def delete_discount(discount_id: str):
+    success = await repository.delete(discount_id)
+    if not success:
+        raise HTTPException(status_code=404, detail="Discount not found")
+    return {"detail": "Discount deleted"}
+
+@router.get("/discounts/", response_model=List[Discount])
+async def list_discounts(limit: int = 100, skip: int = 0):
+    return await repository.list(limit, skip)
