836539780af05bd033bd04be4ff1007a8a852262
itz-amethyst <pransermi@gmail.com>
2024-07-23 23:23:52 +0330

    feat: image captcha

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+import base64
 from datetime import datetime
+import io
+import random
+import string
 from typing import Optional
 from uuid import UUID
 from fastapi import Cookie, FastAPI, Request, Response
-
+from captcha.image import ImageCaptcha
+from fastapi.responses import StreamingResponse
+from starlette.middleware.sessions import SessionMiddleware
 
 cookies = dict()
-app = FastAPI()
+app = FastAPI(docs_url='/docs')
+app.add_middleware(SessionMiddleware,secret_key = "Some-secret-key")
 
 @app.post("set_cookie/")
 def create_cookie(rep:Response,id :UUID, username: str ):
@@ -20,7 +27,7 @@ def get_cookie(userkey: Optional[str] = Cookie(None), identifier: Optional[str] 
     return cookies
 
 @app.middleware("http")
-async def log_transaction_filter(request: Request, call_next):
+def log_transaction_filter(request: Request, call_next):
     start_time = datetime.now()
     query_params = request.query_params
     path_params = request.path_params
@@ -32,5 +39,24 @@ async def log_transaction_filter(request: Request, call_next):
         
     response = call_next(request)
     process_time = datetime.now() - start_time
-    request.headers["X-TIME-PROCESS"] = str(process_time)
+    # request.headers["X-TIME-PROCESS"] = str(process_time)
     return response
+
+
+def captcha_generator(size: int):
+    return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(size))
+
+
+def generate_captcha():
+    captcha: str = captcha_generator(5)
+    image = ImageCaptcha()
+    data = image.generate(captcha)
+    data = base64.b64encode(data.getvalue())
+    return {"data": data, "captcha": captcha}
+
+@app.get('/start-session')
+def start_session(request: Request):
+    captcha = generate_captcha()
+    request.session["captcha"] = captcha['captcha']
+    captcha_image = captcha["data"].decode("utf-8")
+    return StreamingResponse(io.BytesIO(base64.b64decode(captcha_image)), media_type="image/png")
