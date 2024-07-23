10b9474c66b2bc593c0cb9eca11aca7569275aa7
itz-amethyst <pransermi@gmail.com>
2024-07-23 23:47:17 +0330

    feat: new settings

---
+++ user_service/config/__init__.py
@@ -0,0 +1,4 @@
+from user_service.config.settings import get_settings
+
+
+settings = get_settings()
---
+++ user_service/config/settings.py
@@ -0,0 +1,21 @@
+from functools import lru_cache
+from pydantic import validator
+from pydantic_settings import BaseSettings
+from common.config.helper import DOTENV
+
+class Settings(BaseSettings):
+    VERIFICATION_METHOD: str = "OTP"
+    PASSWORD_CRYPT_ALGO: str = "bcrypt"
+
+    class Config:
+        env_flie = DOTENV
+    
+    @validator("VERIFICATION_METHOD", pre=True, always=True)
+    def validate_verification_method(cls, value):
+        if value not in ("OPT", "Magic"):
+            return "OTP"
+        return value
+
+@lru_cache(maxsize=1)
+def get_settings() -> Settings:
+    return Settings
