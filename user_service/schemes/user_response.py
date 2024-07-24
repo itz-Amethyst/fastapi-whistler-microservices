dc6203052da684cd60ac6af86c92b8f39b3cf0b0
itz-amethyst <pransermi@gmail.com>
2024-07-24 22:17:52 +0330

    fix: user response schema

--- user_service/schemes/user_response.py
+++ user_service/schemes/user_response.py
@@ -10,33 +10,16 @@ class UserInDBBase(UserBase):
     model_config = ConfigDict(from_attributes=True)
 
 
-# todo decide later 
-# Additional properties stored in DB
-class UserInDB(UserInDBBase):
-    hashed_password: Optional[SecretStr] = None
-    totp_secret: Optional[SecretStr] = None
-    totp_counter: Optional[int] = None
 
-
-class UserReponse(UserInDB):
-    hashed_password: bool = Field(default= False, alias = "password")
+class UserReponse(UserInDBBase):
+    is_verified: bool 
+    username: str
+    email: str
+    is_superuser: bool
+    is_active: bool
+    email_verified: bool
     # totp_secret: bool = Field(default= False, alias = "password")
     model_config = ConfigDict(populate_by_name=True)
-
-    
-    @classmethod
-    @field_validator("hashed_password", model="before")
-    def evaluate_hashed_password(cls, hashed_password):
-        if hashed_password:
-            return True
-        return False
-
-    # @classmethod
-    # @field_validator("totp_secret", model="before")
-    # def evaluate_totp_secret(cls, totp_secret):
-    #     if totp_secret:
-    #         return True
-    #     return False
 
     class Config:
         # used for having multiple response type like single and plural (list)
