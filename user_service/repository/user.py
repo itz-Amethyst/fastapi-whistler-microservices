451957e268006fd60e78cb418e3e2d83cac9ea58
itz-amethyst <pransermi@gmail.com>
2024-07-26 22:32:54 +0330

    refactor: apply password_hash on user create

--- user_service/repository/user.py
+++ user_service/repository/user.py
@@ -10,21 +10,23 @@ from common.dep.db import DBSessionDepAsync
 from product_service.models.product import Product as ProductModel
 from user_service.models.user import User
 from user_service.schemes import UserReponse
+from user_service.utils.security.hash import hash_password
 class UserRepository:
     
     def __init__(self, sess: Session) -> None:
         self.session: Session = sess
-        
+    
+    #! Todo create is_email_verified = false -> send email
+    
     async def get_user_by_id(self, user_id: str) -> Optional[UserReponse]:
-        result = await self.session.execute(select(User).filter_by(id=user_id))
-        return result.scalars().first()
+        return self.session.get(User, user_id)
     
     async def get_user_by_username(self, user_name: str)-> Optional[UserReponse]:
         result = await self.session.execute(select(User).filter_by(username= user_name))
         return result.scalars().first()
 
     async def create_user(self, username: str, password: str, is_superuser: bool = False):
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        hashed_password = hash_password(password)
         new_user = User(id=username, username=username, hashed_password=hashed_password, is_superuser=is_superuser)
         async with self.session() as session:
             async with session.begin():
