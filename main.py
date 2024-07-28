c5fb32f428eedb2e83e9bea4efe8668ad7ad704c
itz-amethyst <pransermi@gmail.com>
2024-07-28 22:18:43 +0330

    feat: user startup create

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ from common.config.logger import configure_logging
 from common.db.session import sessionManager, Base
 from product_service.api import router as product_router
 from common.utils.logger import logger_system
-
+from common.setup.helper.lifespan import lifespan
 # if project gets bigger should decide to create a setup folder for this even 
 configure_logging()
-app = FastAPI() 
+app = FastAPI(lifespan=lifespan) 
 
 app.include_router(router=product_router, prefix='/product')
 
-@asynccontextmanager
-async def lifespan(app):
-    logger_system.info("Starting server ...")
+# @asynccontextmanager
+# async def lifespan_setup(app):
+#     logger_system.info("Starting server ...")
 
-    async with sessionManager._async_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    yield
+#     async with sessionManager._async_engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.create_all)
+#     yield
 
-    logger_system.info("Shutting down the system.")
+#     logger_system.info("Shutting down the system.")
 
-    await sessionManager.close_async()
+#     await sessionManager.close_async()
