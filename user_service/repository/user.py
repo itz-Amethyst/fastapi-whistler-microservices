a2ba6b9e36aeb5d8d6019a2d053e4f8fd52e6607
itz-amethyst <pransermi@gmail.com>
2024-07-28 22:19:02 +0330

    feat: create methods for user repo

--- user_service/repository/user.py
+++ user_service/repository/user.py
@@ -8,14 +8,16 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from datetime import datetime
 from common.dep.db import DBSessionDepAsync
 from product_service.models.product import Product as ProductModel
+from user_service.models.scope import Scope
 from user_service.models.user import User
 from user_service.schemes import UserReponse
+from user_service.schemes.user import SuperUserCreate, UserCreate
 from user_service.utils.security.hash import hash_password
 class UserRepository:
     
     def __init__(self, sess: Session) -> None:
         self.session: Session = sess
-    
+
     #! Todo create is_email_verified = false -> send email
     
     async def get_user_by_id(self, user_id: str) -> Optional[UserReponse]:
@@ -25,12 +27,41 @@ class UserRepository:
         result = await self.session.execute(select(User).filter_by(username= user_name))
         return result.scalars().first()
 
-    async def create_user(self, username: str, password: str, is_superuser: bool = False):
-        hashed_password = hash_password(password)
-        new_user = User(id=username, username=username, hashed_password=hashed_password, is_superuser=is_superuser)
+    async def create_user(self, user_data: UserCreate) -> Optional[UserReponse]:
+        hashed_password = await hash_password(user_data.password)
+        new_user = User(
+            username=user_data.username,
+            email=user_data.email,
+            password_hash=hashed_password,
+            email_verified=user_data.email_verified,
+            is_active=user_data.is_active,
+            is_superuser=user_data.is_superuser
+        )
         async with self.session() as session:
             async with session.begin():
                 session.add(new_user)
+                await session.commit()
+                await session.refresh(new_user)
+        return new_user
+
+    async def create_super_user(self, user_data: SuperUserCreate) -> User:
+        new_user = await self.create_user(user_data)
+        
+        # Add full_permission scope
+        async with self.session() as session:
+            scope = await session.execute(select(Scope).where(Scope.name == 'full_permission'))
+            full_permission_scope = scope.scalar()
+            if not full_permission_scope:
+                full_permission_scope = Scope(name='full_permission')
+                session.add(full_permission_scope)
+                await session.commit()
+                await session.refresh(full_permission_scope)
+            
+            new_user.scopes.append(full_permission_scope)
+            await session.commit()
+            await session.refresh(new_user)
+            
+        return new_user
 
     async def get_by_email(self, email: str) -> Optional[UserReponse]:
         result = await self.session.execute(select(User).filter_by(email=email))
