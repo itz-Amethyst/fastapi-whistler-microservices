4c4d25f62bf23ea9b57405ed53d8d5c60e817751
itz-amethyst <pransermi@gmail.com>
2024-07-28 22:18:43 +0330

    feat: user startup create

--- user_service/schemes/user.py
+++ user_service/schemes/user.py
@@ -1,29 +1,37 @@
-from pydantic import EmailStr, StringConstraints
+from pydantic import EmailStr, StringConstraints, BaseModel
 from common.schemes.base import BaseModel
 from typing import Annotated, Optional
 from datetime import datetime
 
+# class UserBase(BaseModel):
+#     email: Optional[EmailStr] = None
+#     email_verified: Optional[bool] = False
+#     is_superuser: Optional[bool] = False
+#     is_active: Optional[bool] = True
+#     user_name: Optional[str] = ""
 
-class UserBase(BaseModel):
-    email: Optional[EmailStr] = None
-    email_verified: Optional[bool] = False
-    is_superuser: Optional[bool] = False
-    is_active: Optional[bool] = True
-    user_name: Optional[str] = ""
-
-class UserCreate(UserBase):
+class UserCreate(BaseModel):
     username: str
     email: EmailStr
-    password: Optional[Annotated[str, StringConstraints(min_length=8, max_length=64)]] = None
+    password: str
+    email_verified: Optional[bool] = False
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+
+class SuperUserCreate(UserCreate):
+    is_superuser: bool = True
     
 
-class UserUpdate(UserBase):
-    original: Optional[Annotated[str, StringConstraints(min_length=8, max_length=64)]] = None
-    new_password: Optional[Annotated[str, StringConstraints(min_length=8, max_length=64)]] = None
+class UserUpdate(BaseModel):
+    original: Annotated[str, StringConstraints(min_length=8, max_length=64)]
+    new_password: Annotated[str, StringConstraints(min_length=8, max_length=64)]
 
     
-class UserLogin(UserBase):
+class UserLogin(BaseModel):
     username: str
     # login through username or email decide later
     # email: EmailStr
     password: str
+
+class UserVerify(BaseModel):
+    token: str
--- user_service/setup/helper/init_db.py
+++ user_service/setup/helper/init_db.py
@@ -1,10 +1,12 @@
 from pymongo.database import Database
-
-from app import crud , schemas
-from app.config.settings import settings
-from app.utils.logger import logger_system
+from user_service.repository.user import user_repository
+from user_service.schemes import UserCreate 
+from common.utils.logger import logger_system
+from user_service.config import settings
 from tenacity import retry, stop_after_attempt, wait_fixed
-FIRST_SUPERUSER = settings.general.FIRST_SUPERUSER
+from user_service.utils.security.hash import hash_password
+
+FIRST_SUPERUSER = settings.FIRST_SUPERUSER_NAME
 
 max_tries = 60 * 5  # 5 minutes
 wait_seconds = 1
@@ -15,16 +17,19 @@ wait_seconds = 1
     wait=wait_fixed(wait_seconds),
 )
 async def init_db(db: Database) -> None:
+    # Todo
     print("inside")
     logger_system.warning("Creating initial data")
-    user = await crud.user.get_by_email(db, email=FIRST_SUPERUSER)
+    user = await user_repository.get_user_by_username(user_name=settings.FIRST_SUPERUSER_NAME)
     if not user:
+        hashed_password = hash_password(settings.FIRST_SUPERUSER_PASSWORD)
         # Create user auth
-        user_in = schemas.UserCreate(
+        user_data = UserCreate(
             email=FIRST_SUPERUSER,
-            password=settings.general.FIRST_SUPERUSER_PASSWORD,
+            password=hashed_password,
             is_superuser=True,
-            full_name=FIRST_SUPERUSER,
+            email_verified=True,
+            username=FIRST_SUPERUSER,
         )
-        user = await crud.user.create(db, obj_in=user_in)  # noqa: F841
+        user = await user_repository.create_super_user(user_data) 
     logger_system.warning("Initial data created")
