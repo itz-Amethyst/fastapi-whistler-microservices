5685a6bf0571936e855f3a99bb2b2c6a291ea861
itz-amethyst <pransermi@gmail.com>
2024-07-31 23:05:29 +0330

    refactor: improved base class to encounter mapper props

--- common/models/base.py
+++ common/models/base.py
@@ -1,15 +1,29 @@
+from typing import TYPE_CHECKING
 from common.db.session import Base
 from datetime import datetime
 from sqlalchemy import DateTime, Integer
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+from sqlalchemy.ext.declarative import declared_attr 
 
 def date_time_sec():
     return datetime.now().replace(microsecond=0)
 
-
-class BaseModel(Base):
+class BaseModel(DeclarativeBase):
     __abstract__ = True
     
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
-    created_time: Mapped[datetime] = mapped_column(DateTime, default=date_time_sec, nullable=False)
-    modified: Mapped[datetime] = mapped_column(DateTime, default=date_time_sec, onupdate=date_time_sec, nullable=True)
+
+    # @declared_attr
+    # def id(self) -> Mapped[int]:
+    #     return mapped_column(primary_key=True)
+
+    # if TYPE_CHECKING:
+    #     created_time: Mapped[datetime] = mapped_column(DateTime, default=date_time_sec, nullable=False)
+    #     modified: Mapped[datetime] = mapped_column(DateTime, default=date_time_sec, onupdate=date_time_sec, nullable=True)
+    @declared_attr
+    def created_time(self) -> Mapped[datetime]:
+        return mapped_column(DateTime, default=date_time_sec, nullable=False)
+    
+    @declared_attr
+    def modified(self) -> Mapped[datetime]:
+        return mapped_column(DateTime, default=date_time_sec, onupdate=date_time_sec, nullable=True)
