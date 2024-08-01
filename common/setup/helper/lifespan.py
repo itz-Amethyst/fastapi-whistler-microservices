68ce09858e431f8dec103b368c4c3ad600e1f95e
itz-amethyst <pransermi@gmail.com>
2024-08-01 20:46:38 +0330

    fix: logging and lifespan

--- common/setup/helper/lifespan.py
+++ common/setup/helper/lifespan.py
@@ -1,15 +1,26 @@
 from contextlib import asynccontextmanager
-from common.db.session import sessionManager
+
+from sqlalchemy import text
+from common.db.session import sessionManager, DBSessionManager
 from fastapi import FastAPI
 from common.utils.logger import logger_system
-from common.db.session import Base
+from common.db.session import Base, metadata, DATABASE_ENGINE, DATABASE_ENGINE_ASYNC
+import logging 
+
+logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    logger.info("PRINTINGGGGG")
     logger_system.info("Starting server ...")
 
-    async with sessionManager._async_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    with sessionManager.session_sync() as conn:
+
+        # conn.execute(text("DROP SCHEMA public CASCADE; CREATE SCHEMA public;"))
+        Base.metadata.create_all(bind=conn.get_bind())
+        conn.commit()
+    # async with sessionManager._async_engine.begin() as conn:
+    #     await conn.run_sync(Base.metadata.create_all)
     yield
 
     logger_system.info("Shutting down the system.")
