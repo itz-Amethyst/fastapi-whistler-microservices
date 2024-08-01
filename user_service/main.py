4000267a0142a9e561452315fd4287ccec304a2d
itz-amethyst <pransermi@gmail.com>
2024-08-01 20:46:38 +0330

    fix: logging and lifespan

--- user_service/main.py
+++ user_service/main.py
@@ -1,9 +1,14 @@
 from user_service import api
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from common.setup.helper.lifespan import lifespan
+from common.config import settings
+from common.utils.logger import logger_system
+from common.config.logger import configure_logging
 
+configure_logging()
 
-app = FastAPI(docs_url='/docs/')
+app = FastAPI(docs_url='/docs/', lifespan=lifespan, debug=True)
 app.add_middleware(
             CORSMiddleware,
             allow_origins=["*"],
