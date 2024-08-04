bc9a60a35bbf94be683524fef1ed6b1f9dc78dfb
itz-amethyst <pransermi@gmail.com>
2024-08-04 22:56:18 +0330

    feat: add full_permission by default

--- user_service/utils/scope_update.py
+++ user_service/utils/scope_update.py
@@ -1,13 +1,13 @@
 import os
 from typing import List
-from common.dep.db import DBSessionDep
+from common.db.session import get_db_session_sync
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 from common.utils.logger import logger_system
 
 from user_service.models.scope import Scope
 
-def get_service_directories() -> list[str]:
+def get_service_directories() -> List[str]:
     current_dir = os.getcwd()
     return [
         dir_name for dir_name in os.listdir(current_dir)
@@ -19,7 +19,7 @@ def update_scopes(db: Session, service_dirs: List[str]):
     existing_scopes_names = {scope.name for scope in existing_scopes}
 
     for service_dir in service_dirs:
-        if service_dirs not in existing_scopes_names:
+        if service_dir not in existing_scopes_names:
             try:
                 new_scope = Scope(name=service_dir, description= f"Scope for {service_dir}")
                 db.add(new_scope)
@@ -28,10 +28,19 @@ def update_scopes(db: Session, service_dirs: List[str]):
                 logger_system.info(f"Added new scope for {service_dir}")
             except IntegrityError:
                 print(f"Scope already exists: {service_dir}")
+    if "full_permission" not in existing_scopes:
+        try:
+            new_scope = Scope(name="full_permission", description="Full permission scope")
+            db.add(new_scope)
+            db.commit()
+            logger_system.info("Added full_permission scope")
+        except IntegrityError:
+            logger_system.warning("full_permission scope already exists") 
 
-async def check_and_update_scopes(db: Session = DBSessionDep):
+def check_and_update_scopes():
+    db = get_db_session_sync()
     try:
         service_dirs = get_service_directories()
-        update_scopes(db, service_dirs)
+        update_scopes(next(db), service_dirs)
     except Exception as e:
         logger_system.error(f"something went wrong during updating scopes {e}")
