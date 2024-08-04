3282cac36f4809a156d2787565e7256547b28297
itz-amethyst <pransermi@gmail.com>
2024-08-04 22:59:51 +0330

    feat: refactor and verify func for user api

--- user_service/api/user.py
+++ user_service/api/user.py
@@ -1,29 +1,45 @@
 from datetime import timedelta
-from typing import Dict
+from typing import Dict, List, Optional, Tuple, Union
 from fastapi import APIRouter, Depends, HTTPException, Security, BackgroundTasks
 from fastapi.responses import JSONResponse
 from common.utils.email import send_email_validation_email 
 from user_service.repository.user import UserRepository
 from user_service.schemes import UserCreate
-from sqlalchemy import Tuple
 from sqlalchemy.ext.asyncio import AsyncSession
 from common.dep.db import DBSessionDepAsync
-from user_service.utils.email import create_email_verification_token
+from common.db.session import get_db_session_async
+from user_service.utils.email import create_email_verification_token, decode_email_verification_token
 from user_service.utils.security.auth import AuthDependency 
 from user_service.schemes import UserResponse, TokenResponse, EmailValidation
 from user_service.models.user import User
 from common.config import settings
 
 router = APIRouter()
+db: AsyncSession = get_db_session_async()
 
 @router.get("/me", response_model=UserResponse)
-async def get_me(user: User = Security(AuthDependency(session=DBSessionDepAsync))):
+async def get_me(user: User = Security(AuthDependency())):
     return user
 
 # Todo anotation
 @router.get("/current", response_model=TokenResponse)
-async def get_current_token(auth_data = Security(AuthDependency(session=DBSessionDepAsync,return_token=True))):
-    return auth_data[1]
+async def get_current_token(auth_data = Security(AuthDependency(return_token=True))):
+    return TokenResponse(access_token=auth_data[1], token_type="Bearer")
+
+@router.get("/getall", response_model=List[UserResponse])
+async def get_all_users(
+    auth_data: Union[None, Tuple[Optional[User], str]] = Security(
+        AuthDependency(token_required=True, return_token=False), scopes=["full_control"]
+    ),
+    db: AsyncSession = DBSessionDepAsync):
+    user, token = auth_data
+    
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid authentication credentials")
+    
+    user_repo = UserRepository(db)
+    result = await user_repo.retrieve_all_users()
+    return result
 
 
 @router.post('/create', response_model=Dict[str, str])
@@ -37,9 +53,9 @@ async def create_user(user: UserCreate, tasks: BackgroundTasks, db: AsyncSession
         email=user.email,
         password=user.password,
     )
-    new_user_id = await user_repo.create_user(new_user)
+    new_user = await user_repo.create_user(new_user)
 
-    token = create_email_verification_token(user_id=str(new_user_id), expires_delta=timedelta(hours=12))
+    token = create_email_verification_token(user_id=str(new_user.id), expires_delta=timedelta(hours=12))
     email_validation_data = EmailValidation(email=user.email, subject="Verify Your Account", token=token)
     tasks.add_task(send_email_validation_email, email_validation_data)
     # Todo dict response 200, ok
@@ -62,3 +78,14 @@ async def resend_verification(email: str, tasks: BackgroundTasks, db: AsyncSessi
     email_validation_data = EmailValidation(email=user.email, subject="Verify Your Account", token=token)
     tasks.add_task(send_email_validation_email, email_validation_data)
     return {"message": "Verification email has been resent. Please check your email."}
+
+@router.get('/verify', response_model=Dict[str, str])
+async def verify_account(token: str, db: AsyncSession = DBSessionDepAsync):
+    user_repo = UserRepository(db)
+    decoded_payload =  decode_email_verification_token(token)
+    user = await user_repo.get_user_by_id(user_id=decoded_payload['sub'])
+    if not user:
+        raise HTTPException(422, "Invalid code")
+    
+    await user_repo.update_user_verification(int(decoded_payload['sub']))
+    return {"message": "Successfully verified the account"}
