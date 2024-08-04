3ae74e4b0b36e076108d09c134ed7396c3c5706c
itz-amethyst <pransermi@gmail.com>
2024-08-04 23:39:10 +0330

    fix: to show relations title in response

--- user_service/schemes/user_response.py
+++ user_service/schemes/user_response.py
@@ -21,9 +21,27 @@ class UserResponse(BaseModel):
     # tfa_enabled: bool
     # totp_secret: bool = Field(default= False, alias = "password")
     scopes: Optional[List[str]] = []
-    products: Optional[List]
+    products: Optional[List[str]]
+    
+
+    @classmethod
+    def from_orm(cls, obj):
+        # Create a dictionary of the object's attributes
+        obj_dict = {
+            key: getattr(obj, key) 
+            for key in cls.model_fields.keys() 
+            if hasattr(obj, key)
+        }
+        
+        # Manually process scopes and products
+        obj_dict['scopes'] = [scope.name for scope in obj.scopes]
+        obj_dict['products'] = [product.title for product in obj.products]
+        
+        return cls(**obj_dict)
+
     
     class Config:
         # used for having multiple response type like single and plural (list)
         populate_by_name = True
         orm_mode = True
+        from_attributes=True
