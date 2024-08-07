9f8ce6e087593c362d4a87fa34e9e40d035eac71
itz-amethyst <pransermi@gmail.com>
2024-08-07 23:00:29 +0330

    feat: dynamic import service routers

--- common/setup/helper/route.py
+++ common/setup/helper/route.py
@@ -1,13 +1,27 @@
+from importlib import import_module
 from user_service import api as user_api
 from order_service import api as order_api
 from discount_service import api as discount_api
 from product_service import api as product_api
 from fastapi import FastAPI
+from common.utils.logger import logger_system
+from user_service.utils.scope_update import get_service_directories
 
 
 def setup_routers(app: FastAPI) -> None:
+    service_directories = get_service_directories()
     
-    app.include_router(user_api.router)
-    app.include_router(order_api.router)
-    app.include_router(discount_api.router)
-    app.include_router(product_api.router)
+    for service_dir in service_directories:
+        try:
+            module_path = f"{service_dir}.main"
+            service_module = import_module(module_path)
+            
+            if hasattr(service_module, "app") and hasattr(service_module.app, "router"):
+                app.include_router(service_module.app.router, prefix=f"/{service_dir}")
+            else:
+                logger_system.error(f"Module {module_path} does not have app attribute and route associated with it")
+        except ModuleNotFoundError:
+            logger_system.error(f"Module {module_path} not found, skipping...")
+        except Exception as e:
+            logger_system.error(f"Failed to include router {module_path}")
+    
