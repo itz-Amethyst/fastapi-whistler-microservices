f477bbcab23b0d520f1b25e3253deae77b22c19c
itz-amethyst <pransermi@gmail.com>
2024-08-07 15:18:28 +0330

    fix: moved scopes to session

--- user_service/utils/security/token.py
+++ user_service/utils/security/token.py
@@ -1,22 +1,28 @@
-from fastapi import Response
+from fastapi import Response, Request
 from user_service.config import settings
 from datetime import datetime, timedelta
 from typing import Any, Union
 import jwt
 
 
-def create_access_token(subject: Union[str, Any], scopes: list[str] = None, expires_delta: timedelta = None) -> str:
+def create_access_token(subject: Union[str, Any], scopes: list[str] = None, expires_delta: timedelta = None, request: Request = None) -> str:
     if expires_delta is None:
         expires_delta = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     expire = datetime.utcnow() + expires_delta
-    to_encode = {"exp": expire, "sub": str(subject), "scopes": scopes or []}
+    to_encode = {"exp": expire, "sub": str(subject)}
+    if request:
+        request.session['scopes'] = scopes or []
+    else:
+        to_encode['scopes'] = scopes or [] 
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
 
 
-def decode_access_token(token: str) -> dict:
+def decode_access_token(token: str, request: Request) -> dict:
     try:
         decoded_jwt = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        scopes = request.session.get("scopes", [])
+        decoded_jwt['scopes'] = scopes
         return decoded_jwt
     except jwt.ExpiredSignatureError:
         raise ValueError("Token has expired")
