0c32d2598cd7b21b2658815de76f25560b3d1244
itz-amethyst <pransermi@gmail.com>
2024-08-10 00:20:46 +0330

    chore: product repository

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
 from common.setup.factory import create_app
-# if project gets bigger should decide to create a setup folder for this even 
-# app = FastAPI(lifespan=lifespan) 
+
 app = create_app()
-
-
-# @asynccontextmanager
-# async def lifespan_setup(app):
-#     logger_system.info("Starting server ...")
-
-#     async with sessionManager._async_engine.begin() as conn:
-#         await conn.run_sync(Base.metadata.create_all)
-#     yield
-
-#     logger_system.info("Shutting down the system.")
-
-#     await sessionManager.close_async()
--- product_service/repository/product.py
+++ product_service/repository/product.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 from fastapi import UploadFile
 from slugify import slugify
 from sqlalchemy import delete, update, insert, text
@@ -9,6 +9,7 @@ from common.schemes.image import Image
 from product_service.models.product import Product as ProductModel
 from product_service.schemas import Product 
 from common.repository.genericPicture import GenericPictureRepository
+from product_service.schemas.product import ProductCreate
 
 class ProductRepository:
     
@@ -16,31 +17,49 @@ class ProductRepository:
         self.session: Session = sess
         self.generic_picture_repo = GenericPictureRepository(session=sess)
 
+    async def product_name_exists(self, name: str) -> bool:
+        sql = text(
+            """
+                SELECT COUNT(*) FROM products WHERE name = :name
+            """
+        )
+        result = await self.session.execute(sql, {"name": name})
+        count = result.scalar()
+        return count > 0
 
     #fix seller_id later to retrieve real data
-    async def insert_product(self, seller_id: int, product, picture: UploadFile = None) -> bool:
+    async def insert_product(self, seller_id: int, product: ProductCreate, picture: UploadFile = None) -> Union[Any, bool]:
         try:
-            product.slug = slugify(product.name)
+            if await self.product_name_exists(product['name']):
+                print(f"Product with name '{product['name']}' already exists.")
+                return f"Product with name {product['name']} already exists. ", False
+        
+            product_data = dict(product)
+            product_data['slug'] = slugify(product['name'])
+            product_data['seller_id'] = seller_id
             sql = text(
                 """
-                    insert into products (name, slug, description, price, seller_id)
-                    values(:name, :slug, :description, :price, :seller_id)
+                    insert into products (name, slug, description, price, seller_id, is_deleted)
+                    values(:name, :slug, :description, :price, :seller_id, :is_deleted)
                     returning id
                 """) 
-            result = await self.session.execute(sql,{**product , "seller_id": seller_id})
+            result = await self.session.execute(sql, {**product_data, "is_deleted": False})
             product_id = result.fetchone()[0]
             
             if picture:
                 await self.generic_picture_repo.add_picture(product_id, picture, model_name = ProductModel.__tablename__)
 
             await self.session.commit()
-
-            data = await self.get_product_with_pictures(product_id)
+            if picture:
+                
+                data = await self.get_product_with_pictures(product_id)
+            else:
+                data = await self.get_product_by_id(product_id)
             return data, True
         except Exception as e :
             await self.session.rollback()
             print(f"Error while creating product: {e}")
-            return False
+            return [], False
             
     async def get_all_products(self, skip:int = 0 , limit:int = 10) ->List[Product]:
         try:
@@ -55,6 +74,7 @@ class ProductRepository:
 
     async def update_product(self, product_id: int, details: Dict[str, Any] , picture: UploadFile = None) -> Product:
         try:
+
             set_clause = ", ".join([f"{key} = :{key}" for key in details.keys()])
             sql = text(
                     f"""
@@ -102,7 +122,7 @@ class ProductRepository:
         try:
             sql = text(
                 """
-                select p.id, p.name, p.slug, p.descriptionSELECT p.id, p.name, p.slug, p.description, p.price, p.seller_id,
+                select p.id, p.name, p.slug, p.description, p.description, p.price, p.seller_id, p.created_time, p.modified, p.is_deleted,
                        gp.picture_url, gp.content_type_id, gp.object_id
                 from products p
                 left join generic_pictures gp on p.id = gp.object_id
@@ -111,45 +131,49 @@ class ProductRepository:
             )
             
             result = await self.session.execute(sql, {"product_id": product_id})
-            rows = result.fetchall()
+            # Execute the query and get all rows with mappings
+            rows = result.mappings().all()
 
             if not rows:
                 return None
             
-            product_data = rows[0]
-           
-            product_dict = {k: product_data[k] for k in product_data.keys() if k != "picture_url"} 
-            
-            product_dict['images'] = [] 
+
+            # Return as Product model
+            current_product_dict = None
             
             for row in rows:
+                current_product_dict = {k: row[k] for k in row.keys() if k != "picture_url"}
+                current_product_dict['images'] = []
+
                 if row['picture_url']:
                     image = Image(
-                        picture_url = row['picture_url'],
+                        picture_url=row['picture_url'],
                         content_type_id=row["content_type_id"],
-                        object_id = row['object_id']
+                        object_id=row['object_id']
                     )
-                    product_dict['images'].append(image)
-                    
-            return Product(**product_dict)
+                    current_product_dict['images'].append(image)
+            
+            return Product(**current_product_dict)
         except Exception as e:
             await self.session.rollback()
-            print("Something went wrong while fetching product with pictures: {e}")
+            print(f"Something went wrong while fetching product with pictures: {e}")
             return None
         
-    async def get_all_products_with_pictures(self) -> List[Product]:
+    async def get_all_products_with_pictures(self, skip: int = 0,limit: int = 10) -> List[Product]:
         try:
             sql = text(
                 """
-                select p.id, p.name, p.slug, p.description, p.price, p.seller_id,
+                select p.id, p.name, p.slug, p.description, p.price, p.seller_id, p.modified, p.created_time, p.is_deleted,
                        gp.picture_url, gp.content_type_id, gp.object_id
                 from products p
                 left join generic_pictures gp on p.id = gp.object_id
+                order by p.id
+                offset :skip limit :limit
                 """
             )
             
-            result = await self.session.execute(sql)
-            rows = result.fetchall()
+            result = await self.session.execute(sql, {"skip": skip, "limit": limit})
+            rows = result.mappings().all()
 
             products = []
             current_product_id = None
@@ -180,31 +204,42 @@ class ProductRepository:
             print(f"Something went wrong while fetching all products with pictures: {e}")
             return []
     
-    async def get_product_by_slug(self, slug: str) -> Product:
+    async def get_product_by_slug(self, slug: str) -> Optional[int]:
         try:
             sql = text(
                 """
-                select * from products
-                where slug = :slug 
+                SELECT id FROM products
+                WHERE slug = :slug
                 """
             )
-            
             result = await self.session.execute(sql, {"slug": slug})
-        
-            return result.scalar_one()
+            product = result.scalar_one_or_none()
+            return product if product is not None else None
         except Exception as e:
-            await self.session.rollback()
-            print(f"something went wrong while deleting a product: {e}")
+            print(f"Something went wrong while fetching product by slug: {e}")
+            return None
     
-    async def get_product_by_id(self, product_id: int) -> Product:
-        sql = text(
-            """
-            select * from products
-            where id = :product_id
-            """
-        ) 
+    # without images
+    # async def get_product_by_id(self, product_id: int) -> Product:
+    #     try:
+    #         sql = text(
+    #             """
+    #             select * from products
+    #             where id = :product_id
+    #             """
+    #         ) 
 
-        result = await self.session.execute(sql, {"product_id": product_id})
+    #         result = await self.session.execute(sql, {"product_id": product_id})
+    #         product_row = result.fetchone()  # Fetch a single row
+            
+    #         if product_row is None:
+    #             raise Exception(f"Product with ID {product_id} not found.")
+            
+    #         # Convert the row to a dictionary
+    #         columns = result.keys()  # Get column names
+    #         product_data = dict(zip(columns, product_row))
         
-        return result.scalar_one()
-    
+    #         return product_data
+    #     except Exception as e:
+    #         print(f"Error while fetching product: {e}")
+    #         return None
