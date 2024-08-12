4d204d11732656893e98f9f7bc42b1de16838ae1
itz-amethyst <pransermi@gmail.com>
2024-08-12 23:32:45 +0330

    fix: migration script

--- scripts/run_migration.sh
+++ scripts/run_migration.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 set -e
-
+cd "$(dirname "$0")/.."
 echo "Starting Docker containers..."
-docker-compose up -d
+docker-compose up -d db
 
 function wait_for_postgres() {
   echo "Waiting for PostgreSQL to be ready..."
 
-  until pg_isready -h db -U "$POSTGRES_USER"; do
+  until docker-compose exec -T db pg_isready -U "itz-amethyst" -d "whistler-db"; do
     echo "Waiting for PostgreSQL to be ready..."
     sleep 2
   done
@@ -17,10 +17,16 @@ function wait_for_postgres() {
 wait_for_postgres
 
 
-echo "getting alembic revision"
+# echo "getting alembic revision"
 alembic revision --autogenerate -m "init"
 
-LATEST_REVESION_FILE=$(ls code/alembic/versions/*.py | head -n 1)
+if [ -d "code/alembic/versions" ]; then
+    # If 'code' directory exists
+    LATEST_REVISION_FILE=$(ls code/alembic/versions/*.py | head -n 1)
+else
+    # If 'code' directory does not exist
+    LATEST_REVISION_FILE=$(ls alembic/versions/*.py | head -n 1)
+fi
 
 echo "Checking if the migration file is valid"
 if grep -q "def downgrade" "$LATEST_REVESION_FILE" && ! grep -q "pass" "$LATEST_REVESION_FILE"; then
@@ -30,6 +36,7 @@ else
   exit 1
 fi
 
+sleep 1
 alembic upgrade head
 
 echo "run-migration script completed successfully."
