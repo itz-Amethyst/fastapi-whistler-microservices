58bf9c9ccf9a3aa9afd7d7e6b3bd2434f0196854
itz-amethyst <pransermi@gmail.com>
2024-08-17 14:57:40 +0330

    refactor: remove unnecessary fields

--- common/models/pictureMixIn.py
+++ common/models/pictureMixIn.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, and_
+from sqlalchemy import Column, ForeignKey, Integer, and_, select
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.declarative import declared_attr 
@@ -9,20 +9,26 @@ from common.models.genericPictures import GenericPictures
 class PictureMixin:
     # decide later to remove this fields or not
     # Note: without declared's it's gonna throw and error due to abstract rule
-    @declared_attr
-    def content_type_id(cls):
-        return Column(Integer, ForeignKey("content_types.id"), nullable=True)
+    # @declared_attr
+    # def content_type_id(cls):
+    #     return Column(Integer, ForeignKey("content_types.id"), nullable=True)
     
-    @declared_attr
-    def object_id(cls):
-        return Column(Integer, nullable=True)
+    # @declared_attr
+    # def object_id(cls):
+    #     return Column(Integer, nullable=True)
 
     @declared_attr
     def images(cls):
-         return relationship(
+        model_name = cls.__tablename__
+        content_type_id = (
+            select(ContentType.id)
+            .where(ContentType.model == model_name)
+            .scalar_subquery()
+        )
+        return relationship(
             "GenericPictures",
             primaryjoin=lambda: and_(
-                cls.content_type_id == GenericPictures.content_type_id,
+                content_type_id == GenericPictures.content_type_id,
                 cls.id == GenericPictures.object_id
             ),
             foreign_keys=[GenericPictures.content_type_id, GenericPictures.object_id],
